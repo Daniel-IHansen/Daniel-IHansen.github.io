e5cae22bb46307b98c9494dfea4721223afa450d
Daniel Hansen <danielhansendaniel@icloud.com>
2023-03-09 12:50:06 +0100

    commit

--- slideshow.js
+++ slideshow.js
@@ -1,4 +1,4 @@
-//turns the height of the gradients to the height of the pictures constantly
-setInterval(() => {
-    console.log(document.getElementById("firstExplorerImg").style.height)
-}, 5);
+// //turns the height of the gradients to the height of the pictures constantly
+// setInterval(() => {
+//     console.log(document.getElementById("firstExplorerImg").style.height)
+// }, 5);
