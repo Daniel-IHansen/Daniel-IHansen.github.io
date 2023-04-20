c79bd15aebd7d1630e46c1b39a9943513e9510e3
Daniel Hansen <danielhansendaniel@icloud.com>
2023-04-20 11:14:40 +0200

    commit

--- animations.js
+++ animations.js
@@ -1,14 +1,56 @@
-function img1(){
+function menuStart(){
   console.log("test")
   anime({
-    targets: '#img1',
+    targets: '#img5',
+    translateY: -1100,
+    translateX: -110,
+    scale: 2.5,
+    rotate: '90',
     easing: 'easeInOutQuint',
-    keyframes: [
-      {translateY: -40},
-      {translateX: 250, duration: 2000},
-      {translateY: 40},
-      {translateX: 0},
-      {translateY: 0}
-    ],
+    duration: 750,
   });
+  setTimeout(() => {
+    anime({
+      targets: '#img1',
+      translateY: -1320,
+      translateX: 270,
+      rotate: '100',
+      scale: '1.5',
+      easing: 'easeInOutQuint',
+      duration: 750,
+    });
+    setTimeout(() => {
+      anime({
+        targets: '#img2',
+        translateY: -600,
+        translateX: 300,
+        rotate: '100',
+        scale: '1.2',
+        easing: 'easeInOutQuint',
+        duration: 750,
+      });
+      setTimeout(() => {
+        anime({
+          targets: '#img3',
+          translateY: -500,
+          translateX: 300,
+          rotate: '100',
+          scale: '1.2',
+          easing: 'easeInOutQuint',
+          duration: 750,
+        });
+      }, 100);
+      setTimeout(() => {
+        anime({
+          targets: '#img4',
+          translateY: -400,
+          translateX: 300,
+          rotate: '100',
+          scale: '1.2',
+          easing: 'easeInOutQuint',
+          duration: 750,
+        });
+      }, 100);
+    }, 100);
+  }, 100);
 }
