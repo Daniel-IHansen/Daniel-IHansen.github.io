c4ca87026a7c1aa1b83ce9ebcc8f2f987c1635da
DanielHansensen <danielhansendaniel@icloud.com>
2023-04-21 07:56:18 +0200

    commit

--- JS/JSanimations/menuStart.js
+++ JS/JSanimations/menuStart.js
@@ -1,24 +1,32 @@
+//  translateX = Y axis
+//  translateY = X axis
+
 function menuStart(){
+  //  displays nav elements when animation starts
   document.getElementById("homeButton").style.display = "initial";
+
+  //  animates my own card
   anime({
     targets: '#img5',
-    translateY: -1100,
-    translateX: -110,
-    scale: 2.5,
+    translateY: flexScale(-70).vw,
+    translateX: flexScale(-10).vh,
+    scale: 2,
     rotate: '90',
     easing: 'easeInOutQuint',
     duration: 750,
   });
+  //  animates the nav icon
   setTimeout(() => {
     anime({
       targets: '#img1',
-      translateY: -1320,
-      translateX: 270,
+      translateY: flexScale(-83).vw,
+      translateX: flexScale(40).vh,
       rotate: '100',
       scale: '1.5',
       easing: 'easeInOutQuint',
       duration: 750,
     });
+    
     setTimeout(() => {
       anime({
         targets: '#img2',
@@ -50,6 +58,7 @@ function menuStart(){
             duration: 750,
           });
           setTimeout(() => {
+            //  gradually reveals nav elements
             document.getElementById("homeButton").style.opacity = "1";
           }, 200);
         }, 200);
