74bd3fa67b63a694410a79b5c72e4c348b0cc948
DanielHansensen <danielhansendaniel@icloud.com>
2023-04-21 11:07:11 +0200

    commit

--- JS/JSanimations/menuStart.js
+++ JS/JSanimations/menuStart.js
@@ -4,6 +4,9 @@
 function menuStart(){
   //  displays nav elements when animation starts
   document.getElementById("homeButton").style.display = "initial";
+  document.getElementById("cv").style.display = "initial";
+  document.getElementById("projects").style.display = "initial";
+  document.getElementById("contact").style.display = "initial";
 
   //  animates my own card
   anime({
@@ -59,7 +62,16 @@ function menuStart(){
           setTimeout(() => {
             //  gradually reveals nav elements
             document.getElementById("homeButton").style.opacity = "1";
-          }, 200);
+            setTimeout(() => {
+              document.getElementById("cv").style.opacity = "1";
+              setTimeout(() => {
+                document.getElementById("projects").style.opacity = "1";
+                setTimeout(() => {
+                  document.getElementById("contact").style.opacity = "1";
+                }, 300);
+              }, 300);
+            }, 400);
+          }, 400);
         }, 200);
       }, 200);
     }, 200);
