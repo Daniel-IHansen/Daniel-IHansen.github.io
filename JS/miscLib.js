308433d429d0977517097992bcecb88f7f4a877a
DanielHansensen <danielhansendaniel@icloud.com>
2023-06-20 14:12:15 +0200

    added mediaqueries + collections

--- JS/miscLib.js
+++ JS/miscLib.js
@@ -9,17 +9,6 @@ function getDistance(x1, y1, x2, y2) {
     return Math.sqrt(x * x + y * y);
 }
 
-//  returns position based on id
-function getAttributesById(element) { 
-    const rect = document.getElementById(element).getBoundingClientRect();
-    return {
-      x: rect.left + window.scrollX,
-      y: rect.top + window.scrollY,
-      width: rect.width,
-      height: rect.height
-    };
-  }
-
 //  vw to px converter
 function flexScale(viewportVar) {
   const viewportWidth = window.innerWidth;
@@ -30,11 +19,4 @@ function flexScale(viewportVar) {
     vw,
     vh
   }
-}
-
-//  uses setTimeout to wait
-function wait(time, code){
-  setTimeout(() => {
-    code
-  }, time);
 }
