f760a362de20ad4a0fc45659fa21fd7409fba6c1
DanielHansensen <danielhansendaniel@icloud.com>
2023-06-22 12:08:51 +0200

    changed media query / added collections in js

---
+++ JS/collections.js
@@ -0,0 +1,9 @@
+var photoCol = [
+  ["Name","Date","Filename","Small description"],
+["Winter Mirror","15.12.2022","winterMirror.jpg","A combination of photos I have taken, edited and blended in Photoshop."],
+["Blue Lights","10.1.2023","blueLights.jpg","A test to imitate car lights in Photoshop."],
+["Warm Street","10.1.2023","warmStreet.jpg","Street photo, edited and blended in Photoshop."],
+["Movie Night","10.1.2023","movieNight.jpg","A test combination of three images, using window reflection as a base."],
+["Friends","3.6.2023","friends.jpg","Picture taken of my friends, with me edited in using Photoshop."]
+]
+console.log(photoCol[2][0])
