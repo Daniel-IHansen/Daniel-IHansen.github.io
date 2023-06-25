ebcf79314447524c8f7a1316bcf2641527dcc259
DanielHansensen <danielhansendaniel@icloud.com>
2023-06-25 13:32:52 +0200

    made mobile menu

---
+++ JS/indexCode.js
@@ -0,0 +1,37 @@
+//  displays the desired items
+
+//  under Latest Work
+document.getElementById("workImg").src = "collections/portofolio/" + workCol.slice(-1)[0][2];
+document.getElementById("work1").innerHTML = "🞄 " + workCol.slice(-1)[0][0];
+document.getElementById("work2").innerHTML = "🞄 " + workCol.slice(-2)[0][0];
+document.getElementById("work3").innerHTML = "🞄 " + workCol.slice(-3)[0][0];
+document.getElementById("work4").innerHTML = "🞄 " + workCol.slice(-4)[0][0];
+
+//  under Photographs
+document.getElementById("photo1").src = "collections/photos/" + photoCol.slice(-1)[0][2];
+document.getElementById("photo2").src = "collections/photos/" + photoCol.slice(-2)[0][2];
+document.getElementById("photo3").src = "collections/photos/" + photoCol.slice(-3)[0][2];
+document.getElementById("photo4").src = "collections/photos/" + photoCol.slice(-4)[0][2];
+document.getElementById("photo5").src = "collections/photos/" + photoCol.slice(-5)[0][2];
+document.getElementById("photo6").src = "collections/photos/" + photoCol.slice(-6)[0][2];
+document.getElementById("photo7").src = "collections/photos/" + photoCol.slice(-7)[0][2];
+document.getElementById("photo8").src = "collections/photos/" + photoCol.slice(-8)[0][2];
+document.getElementById("photo9").src = "collections/photos/" + photoCol.slice(-9)[0][2];
+document.getElementById("photo10").src = "collections/photos/" + photoCol.slice(-10)[0][2];
+
+//  under Updates
+document.getElementById("title1").innerHTML = updateCol.slice(-1)[0][0];
+document.getElementById("title2").innerHTML = updateCol.slice(-2)[0][0];
+
+document.getElementById("text1").innerHTML = updateCol.slice(-1)[0][2];
+document.getElementById("text2").innerHTML = updateCol.slice(-2)[0][2];
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function showMenu() {
+    var x = document.getElementById("mobileMenu");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
--- collections/collections.js
+++ collections/collections.js
@@ -25,28 +25,3 @@ var updateCol = [
   ["Wednesday 20, June 2023", 'Test', "This is a test to see how the update text behaves. <br/> Although it might not work, I am prepared for the results."],
   ["Saturday 24, June 2023", 'First Contact', "I'm tired. This website is intricate, and every time I want to work on it, energy surges from me, and I don't want to. Yet, I know it will give me joy seeing a fully fleshed out website. My goal is to work on it tonight."]
 ];
-//  under Latest Work
-document.getElementById("workImg").src = "collections/portofolio/" + workCol.slice(-1)[0][2];
-document.getElementById("work1").innerHTML = "🞄 " + workCol.slice(-1)[0][0];
-document.getElementById("work2").innerHTML = "🞄 " + workCol.slice(-2)[0][0];
-document.getElementById("work3").innerHTML = "🞄 " + workCol.slice(-3)[0][0];
-document.getElementById("work4").innerHTML = "🞄 " + workCol.slice(-4)[0][0];
-
-//  under Photographs
-document.getElementById("photo1").src = "collections/photos/" + photoCol.slice(-1)[0][2];
-document.getElementById("photo2").src = "collections/photos/" + photoCol.slice(-2)[0][2];
-document.getElementById("photo3").src = "collections/photos/" + photoCol.slice(-3)[0][2];
-document.getElementById("photo4").src = "collections/photos/" + photoCol.slice(-4)[0][2];
-document.getElementById("photo5").src = "collections/photos/" + photoCol.slice(-5)[0][2];
-document.getElementById("photo6").src = "collections/photos/" + photoCol.slice(-6)[0][2];
-document.getElementById("photo7").src = "collections/photos/" + photoCol.slice(-7)[0][2];
-document.getElementById("photo8").src = "collections/photos/" + photoCol.slice(-8)[0][2];
-document.getElementById("photo9").src = "collections/photos/" + photoCol.slice(-9)[0][2];
-document.getElementById("photo10").src = "collections/photos/" + photoCol.slice(-10)[0][2];
-
-//  under Updates
-document.getElementById("title1").innerHTML = updateCol.slice(-1)[0][0];
-document.getElementById("title2").innerHTML = updateCol.slice(-2)[0][0];
-
-document.getElementById("text1").innerHTML = updateCol.slice(-1)[0][2];
-document.getElementById("text2").innerHTML = updateCol.slice(-2)[0][2];
