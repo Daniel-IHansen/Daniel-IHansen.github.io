6f52188e923081b0a5d8845dc968be2ba0bab398
DanielHansensen <danielhansendaniel@icloud.com>
2023-08-01 19:52:54 +0200

    package configure, lighting, massive redo

---
+++ redesign/movement.js
@@ -0,0 +1,14 @@
+// Import the necessary modules
+import { camera } from "/redesign/render.js";
+
+// Animates the camera position and rotation
+gsap.set(camera.position, {
+  x: 20,
+  y: 23,
+  z: -20,
+});
+gsap.set(camera.rotation, {
+    x: 0,
+    y: 2.5,
+    z: 0,
+  });
--- redesign/render.js
+++ redesign/render.js
@@ -1,7 +1,16 @@
-//  imports three.js, OrbitalControls and GLTFLoader
-import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+//  NOTE TO SELF: Whenever you want to add more imports SPESEFICALLY from three.js, remember to use the 'https://unpkg.com/three@0.126.1...'!!
+//  main module
+import * as THREE from 'https://unpkg.com/three@0.126.1/build/three.module.js';
+
+//  addons
+import { OrbitControls } from 'https://unpkg.com/three@0.126.1/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'https://unpkg.com/three@0.126.1/examples/jsm/loaders/GLTFLoader.js';
+//  post processing
+import { EffectComposer } from 'https://unpkg.com/three@0.126.1/examples/jsm/postprocessing/EffectComposer.js';
+import { RenderPass } from 'https://unpkg.com/three@0.126.1/examples/jsm/postprocessing/RenderPass.js';
+  //  passes and shaders
+  import { UnrealBloomPass } from 'https://unpkg.com/three@0.126.1/examples/jsm/postprocessing/UnrealBloomPass.js';
+
 
 //  places the room inside the renderer
 const renderer = new THREE.WebGLRenderer();
@@ -11,8 +20,7 @@ document.getElementById("container3D").appendChild(renderer.domElement);
 //  defines the scene and camera
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(30, 30, 0);
-
+export { camera }
 //  finds the middle of the screen, and generates orbitalcontrols
 let mouseX = window.innerWidth / 2;
 let mouseY = window.innerHeight / 2;
@@ -20,6 +28,8 @@ let mouseY = window.innerHeight / 2;
 //  places the oribital controls inside renderer
 let controls;
 controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.04;
 
 //  ROOM GENERATION
 let room;
@@ -27,66 +37,45 @@ let room;
 //  imports a GLTF object
 const loader = new GLTFLoader();
 loader.load('/redesign/models/rom.gltf', function(gltf) {
-    room = gltf.scene;
-    room.scale.set(10, 10, 10);
+  room = gltf.scene;
+  room.scale.set(10, 10, 10);
 
-    //  show mesh configs
-    console.log(room);
+  // create light source material with emission
+  const lightSourceMat1 = new THREE.MeshPhongMaterial({
+    emissive: 0xffffff,
+    emissiveIntensity: 1,
+  });
 
+  // create light source material with emission
+  const lightSourceMat2 = new THREE.MeshPhongMaterial({
+      emissive: 0xffd6a1,
+      emissiveIntensity: 1,
+  });
 
-    // create light source material with emission
-    const lightSourceMat1 = new THREE.MeshPhongMaterial({
-        emissive: 0xffffff,
-        emissiveIntensity: 1,
-    });
+  // assign material to table lamp material
+  room.getObjectByName("Cylinder001").material = lightSourceMat1;
 
-    // create light source material with emission
-    const lightSourceMat2 = new THREE.MeshPhongMaterial({
-        emissive: 0xffd6a1,
-        emissiveIntensity: 1,
-    });
+  // assign material to lightbulb material
+  for (let i = 1; i <= 12; i++) {
+    //  adds an extra "0" in case of name
+    let addressingNull = "";
+    if (i < 10) {
+        addressingNull = "0";
+    }
+    room.getObjectByName(`Sphere0${addressingNull + i}`).material = lightSourceMat2;
+  };
 
-    // assign material to table lamp material
-    room.getObjectByName("Cylinder001").material = lightSourceMat1;
+  // assign material to standing lamp material
+  room.getObjectByName("Cylinder004").material = lightSourceMat1;
 
-    // assign material to lightbulb material
-    for (let i = 1; i <= 12; i++) {
-        //  adds an extra "0" in case of name
-        let addressingNull = "";
-        if (i < 10) {
-            addressingNull = "0";
-        }
-        room.getObjectByName(`Sphere0${addressingNull + i}`).material = lightSourceMat2;
-    };
-
-    // assign material to standing lamp material
-    room.getObjectByName("Cylinder004").material = lightSourceMat1;
-
-    room.castShadow = true;
-    room.traverse((child) => {
-      if (child instanceof THREE.Mesh) {
-        child.castShadow = true;
-      }
-    });
-
-    //  MESH CONFIG
-    
-
-    scene.add(room);
+  scene.add(room);
 });
 
-
 //  LIGHTING
-//  generates a ambient light
-const light = new THREE.AmbientLight(0xffefb5, .05);
-scene.add(light);
 
-// Sun definition, position, and addition
-const sun = new THREE.DirectionalLight(0xffefb5, .3);
-sun.position.set(-80, 20, 100);
-scene.add(sun)
-const helper = new THREE.DirectionalLightHelper(sun, 5);
-scene.add(helper);
+//  generates a ambient light
+const light = new THREE.AmbientLight(0xffefb5, .3);
+scene.add(light);
 
 // defines a class for a point light
 class cusPointLight extends THREE.PointLight {
@@ -116,7 +105,7 @@ const standingLamp = new cusPointLight(0xe2af62, 2, 50, -28, 25, -22);
 //  defines 12 light bulbs
 function lightbulbPlacement() {
     let color = 0xffb04f;
-    let intensity = 1;
+    let intensity = .5;
     let distance = 20;
 
     const lightBulb0 = new cusPointLight(color, intensity, distance, 32.2, 35.5, 35);
@@ -134,22 +123,36 @@ function lightbulbPlacement() {
 };
 lightbulbPlacement();
 
-//  animation loop
+//  POST PROCESSING
+const renderScene = new RenderPass(scene, camera);
+const composer = new EffectComposer(renderer);
+composer.addPass(renderScene);
+
+const bloomPass = new UnrealBloomPass(
+  new THREE.Vector2(window.innerWidth, window.innerHeight),
+  .3,
+  0.5,
+  0.1
+);
+composer.addPass(bloomPass)
+
 function animate() {
+
+    composer.render();
+    controls.update();
     requestAnimationFrame(animate);
 
-    renderer.render(scene, camera);
 }
 animate();
 
-let oldx = 900;
-let oldy = 900;
-window.onmousemove = function(ev) {
-  let changex = ev.x - oldx;
-  let changey = ev.y - oldy;
-  camera.rotation.x += changex/6000;
-  camera.rotation.y -= changey/3600;
+// let oldx = 900;
+// let oldy = 900;
+// window.onmousemove = function(ev) {
+//   let changex = ev.x - oldx;
+//   let changey = ev.y - oldy;
+//   camera.rotation.y -= changex/6000;
+//   camera.rotation.x -= changey/6000;
 
-  oldx = ev.x
-  oldy = ev.y;
-}
+//   oldx = ev.x
+//   oldy = ev.y;
+// }
