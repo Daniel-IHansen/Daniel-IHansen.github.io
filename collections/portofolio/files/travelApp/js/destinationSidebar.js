35393162f89c68238173545120c7e291cc8126ec
DanielHansensen <danielhansendaniel@icloud.com>
2023-09-11 12:37:20 +0200

    made another page

--- collections/portofolio/files/travelApp/js/destinationSidebar.js
+++ collections/portofolio/files/travelApp/js/destinationSidebar.js
@@ -1,19 +1,4 @@
-const sidebar = document.getElementById('infoSidebar');
-const icons = document.querySelectorAll('.sidebarIcon');
-let clickedIcon = null;
-
-icons.forEach(icon => {
-    icon.addEventListener('click', () => {
-        //  check if the sidebar is currently hidden
-        const active = sidebar.classList.contains('sidebarActive');
-
-        //  toggle the sidebar and store the clicked icon's name
-        if (active) {
-            clickedIcon = icon.getAttribute('name');
-            sidebar.classList.remove('sidebarActive');
-            sidebarState = icon.getAttribute('name');
-        } else {
-            sidebar.classList.add('sidebarActive');
-        }
-    });
-});
+function sidebar() {
+    var sidebar = document.getElementById('infoSidebar');
+    sidebar.classList.toggle('sidebarActive');
+}
