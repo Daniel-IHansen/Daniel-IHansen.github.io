bcb59828777aadfa9c131aa1027ab9eb115f4ec4
Daniel-IHansen <danielhansendaniel@icloud.com>
2024-11-13 16:03:39 +0100

    omgjøring av nettside til personlig bruk

---
+++ newSiteTailwind/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    fontFamily: {
+      'sans': ['satoshi, sans-serif'],
+      'serif': ['sentient, serif'],
+    },
+    colors: {
+      'text': '#f6f8f8',
+      'background': '#262b30',
+      'primary': '#e0d4c5',
+      'secondary': '#455367',
+      'accent': '#60cfcf',
+     },
+     
+  },
+  plugins: [],
+}
